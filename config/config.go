7728a192c0e3ab5edecded5afd18581cf522d6f0
IlyaYashkin <drum1lord@gmail.com>
2026-05-29 00:18:49 +0300

    tidied up

--- config/config.go
+++ config/config.go
@@ -3,9 +3,9 @@ package config
 var (
 	ScreenWidth                      = 1920
 	ScreenHeight                     = 1080
-	WorkersNumber                    = 12
+	WorkersNumber                    = 6
 	Gravity                          = 0.0
-	NumberOfCharacters               = 3_000_000
+	NumberOfCharacters               = 2_000_000
 	CharacterMaxSpeed                = 40.0
 	CharacterAccelerationCoefficient = 1.
 	CharacterFrictionCoefficient     = 0.05
